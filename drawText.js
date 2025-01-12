060c40d6d4513a157bd8f00ba7272295b1f2a94b
rodstone <rodtronicsnz@gmail.com>
2025-01-12 20:49:24 +1300

    add font size

--- drawText.js
+++ drawText.js
@@ -1,1 +1,8 @@
-function drawText(screenWidthInCharacters, screenHeightInCharacters) {}
+function drawText(screenWidthInCharacters, screenHeightInCharacters) {
+  var wholeScreenTextBuffer = "";
+  // draw top bar
+  var singleRow = "";
+  for (let index = 0; index < screenWidthInCharacters; index++) {
+    singleRow += "";
+  }
+}
