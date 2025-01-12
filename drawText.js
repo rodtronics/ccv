f3e040c4efca41753db9ffb871af50b5a0ac8494
rodstone <rodtronicsnz@gmail.com>
2025-01-12 21:33:10 +1300

    box
    makes a box

--- drawText.js
+++ drawText.js
@@ -1,8 +1,39 @@
-function drawText(screenWidthInCharacters, screenHeightInCharacters) {
+const titleString = "CRIME COMMITTER V";
+function drawBox(screenWidthInCharacters, screenHeightInCharacters) {
   var wholeScreenTextBuffer = "";
   // draw top bar
   var singleRow = "";
   for (let index = 0; index < screenWidthInCharacters; index++) {
-    singleRow += "";
+    singleRow += "█";
   }
+  // add to buffer with newline too
+  wholeScreenTextBuffer += singleRow + "<br>";
+  //middle rows
+  for (let index = 0; index < screenHeightInCharacters - 2; index++) {
+    singleRow = "";
+
+    // add first column
+    singleRow += "█";
+    // add many blank columns
+    if (index == 0) {
+      singleRow +=
+        "&nbsp".repeat((screenWidthInCharacters - 2 - titleString.length) / 2) +
+        titleString +
+        "&nbsp".repeat((screenWidthInCharacters - 2 - titleString.length) / 2);
+      singleRow += "&nbsp";
+    } else {
+      singleRow += "&nbsp".repeat(screenWidthInCharacters - 2);
+    }
+    //add final column
+    singleRow += "█";
+    //add to buffer
+    wholeScreenTextBuffer += singleRow + "<br>";
+  }
+  singleRow = "";
+  // create final row of bars
+  for (let index = 0; index < screenWidthInCharacters; index++) {
+    singleRow += "█";
+  }
+  wholeScreenTextBuffer += singleRow;
+  return wholeScreenTextBuffer;
 }
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function updateDateTime() {
   const now = new Date();
-  const formattedTime = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  const formattedTime = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", second: "2-digit" });
   const options = { weekday: "short", year: "numeric", month: "short", day: "numeric" };
   const formattedDate = now.toLocaleDateString(options);
   document.getElementById("date-time").textContent = `${formattedDate} ${formattedTime}`;
@@ -9,23 +9,6 @@ function updateDateTime() {
 // Update the date/time initially and every second
 updateDateTime();
 setInterval(updateDateTime, 1000);
-document.addEventListener("DOMContentLoaded", () => {
-  const toggles = document.querySelectorAll(".toggle");
 
-  toggles.forEach((toggle) => {
-    toggle.addEventListener("click", () => {
-      const parentNode = toggle.parentElement;
-      const children = parentNode.querySelector(".children");
-
-      if (children) {
-        if (children.style.display === "none" || !children.style.display) {
-          children.style.display = "block";
-          toggle.textContent = "-"; // Collapse indicator
-        } else {
-          children.style.display = "none";
-          toggle.textContent = "+"; // Expand indicator
-        }
-      }
-    });
-  });
-});
+var newBox = drawBox(80, 25);
+document.getElementById("oneBlock").innerHTML = newBox;
